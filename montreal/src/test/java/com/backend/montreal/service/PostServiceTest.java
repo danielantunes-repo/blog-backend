2107f0ac673f030ee2587dc97076d9a8c621bf2f
danielantunes-repo <dap.junioruvv@gmail.com>
2025-04-30 19:23:52 -0300

    feat: docker

--- montreal/src/test/java/com/backend/montreal/service/PostServiceTest.java
+++ montreal/src/test/java/com/backend/montreal/service/PostServiceTest.java
@@ -7,8 +7,10 @@ import static org.mockito.Mockito.*;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +22,7 @@ import com.backend.montreal.repository.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
 
     @Autowired
